4e67ade9d612bfbe1a959d85cc0dabb560681e94
Aneilpatel05 <anil.patel.gty@gmail.com>
2019-03-12 23:32:58 -0400

    remove dice code and make it configurable

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ setInterval(clearGC, 60 * 1000);
 /** Stream the blockchain for Transactions */
 
  const getTransactions = ops => {
-   console.log("in getTxn getting txns")
+   //console.log("in getTxn getting txns")
    ops.forEach(op => {
      const type = op.op[0];
      const params = op.op[1];
      switch (type) {
        case 'transfer': {
-         /** Find transfer */
+         /** Find transfer transactions in the block */
          const transaction = {
            type: 'transfer',
            from: params.from,
@@ -42,41 +42,13 @@ setInterval(clearGC, 60 * 1000);
            timestamp: Date.parse(op.timestamp) / 1000,
            block: op.block,
          };
-          //console.log('Transfer', JSON.stringify(op));
-         //store to mongodb if from is swapsteem
-         if(transaction.to ==='magicdice' ){
           console.log('Transfer', JSON.stringify(op));
-          //get memo
-          let dicememo = transaction.memo;
-          console.log("memo",dicememo);
-          //parse memo
-          let rollType = dicememo.split(' ')[0];
-          let rollPrediction = dicememo.split(' ')[1];
-          //get unique txnId
-          let txnId = op.trx_id;
-          let block = op.block;
-          //rollDice to generate a random number
-          let rollResult = utils.rollDice(rollPrediction,txnId,block)
-          let win = false;
-          //calculate win/loss
-          if (rollType==='under' && rollResult <= rollPrediction){
-            win = true;
-          } else if (rollType==='under' && rollResult > rollPrediction){
-            win = false;
-          } else if (rollType==='over' && rollResult >= rollPrediction){
-            win = true;
-          } else if (rollType==='over' && rollResult < rollPrediction){
-            win = false;
-          } else {
-            console.log("Invalid Bet")
-          }
-          console.log(dicememo,rollType,rollPrediction,txnId, win)
-          
-          //send the bet payout amount to user
-          //utils.sendPayout('aneilpatel',0.1,'STEEM')
-         }
+         /** Validate transaction against a predefined criteria 
+          * and Do something with the transaction if it meets the criteria */
+         
          break;
-         /** Add Cases for all transactionTYPES */
+         /** Add Cases for transaction accoring to rules */
+
        }
      }
    });
@@ -85,7 +57,7 @@ setInterval(clearGC, 60 * 1000);
  };
 
  const loadBlock = blockNum => {
-   console.log('in blocknum and loading block Num '+blockNum)
+  // console.log('in blocknum and loading block Num '+blockNum)
    utils
      .getOpsInBlock(blockNum, false)
      .then(ops => {
@@ -115,10 +87,9 @@ setInterval(clearGC, 60 * 1000);
              });
            });
        } else {
-         console.log
         Block.findOneAndUpdateAsync({},{'blockNum':blockNum})
         .then(function (block) {
-            console.log("Saving block no :"+block)
+            console.log("Saving last block no :"+block)
             var flag = getTransactions(ops);
             flag === true ? loadNextBlock():loadBlock(blockNum)
           }).catch(ValidationError, function(err) {
@@ -144,16 +115,16 @@ setInterval(clearGC, 60 * 1000);
         nextBlockNum = parseInt(foundItems.blockNum)+1;
         console.log("Now processing next block : #"+nextBlockNum)
        }
-      //let nextBlockNum =  30476467  ;// testing purpose
+  
       utils
         .getGlobalProps()
         .then(globalProps => {
-          console.log("globalProps "+globalProps)
+          //console.log("globalProps "+globalProps)
           const lastIrreversibleBlockNum = globalProps.last_irreversible_block_num;
           if (lastIrreversibleBlockNum >= nextBlockNum) {
-            console.log("calling loadblock()"+nextBlockNum)
+            //console.log("calling loadblock()"+nextBlockNum)
             loadBlock(nextBlockNum);
-            console.log("calling loadblock() done "+nextBlockNum)
+            //console.log("calling loadblock() done "+nextBlockNum)
           } else {
             utils.sleep(2000).then(() => {
               console.log(
